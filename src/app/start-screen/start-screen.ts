d12e9a0ea8a3d2b74654b49a0d0b438f76ba2d20
DanyHudson <mail.d.hudson@gmail.com>
2025-10-02 00:07:17 +0200

    added game component,  added images, first commits

--- src/app/start-screen/start-screen.ts
+++ src/app/start-screen/start-screen.ts
@@ -1,11 +1,24 @@
+
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-start-screen',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './start-screen.html',
   styleUrl: './start-screen.scss'
 })
 export class StartScreen {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {};
+
+  newGame() {
+    // start game
+    console.log('New Game started');
+    this.router.navigate(['/game']);
+  }
 
 }
