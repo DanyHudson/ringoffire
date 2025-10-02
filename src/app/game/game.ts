0a5bae10764168230a5fd363e853ea86a05a7c35
DanyHudson <mail.d.hudson@gmail.com>
2025-10-02 20:49:10 +0200

    trying to add  game data file

--- src/app/game/game.ts
+++ src/app/game/game.ts
@@ -1,6 +1,7 @@
 
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GameData } from 'src/models/game-data';
 
 @Component({
   selector: 'app-game',
@@ -8,8 +9,15 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './game.html',
   styleUrl: './game.scss'
-}) 
+})
 export class Game {
+  pickCardAnimation = false;
+  gameData: GameData = new GameData();
 
+  newGame() { }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+
+  }
 }
-  
---
+++ src/models/game-data.ts
@@ -0,0 +1,7 @@
+export class GameData {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
+   
+}
