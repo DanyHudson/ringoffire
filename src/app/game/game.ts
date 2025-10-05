bd519e3bb3b679c242c3d9ce486b2612af350073
DanyHudson <mail.d.hudson@gmail.com>
2025-10-05 21:53:30 +0200

    following tutorial, trying to fix shuffled card array

--- src/app/game/game.ts
+++ src/app/game/game.ts
@@ -12,14 +12,28 @@ import { GameData } from '../../../src/models/game-data';
 })
 export class Game {
   pickCardAnimation = false;
+  currentCard: string | undefined = '';
   gameData: GameData = new GameData();
 
-  newGame() { 
+  constructor() {
+    this.newGame();
+  }
+
+  newGame() {
     this.gameData = new GameData();
+    console.log(this.gameData);
   }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.gameData.stack.pop();
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
 
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
   }
+
 }
--- src/models/game-data.ts
+++ src/models/game-data.ts
@@ -3,5 +3,31 @@ export class GameData {
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
-   
+
+    constructor() {
+        for (let i = 1; i <= 14; i++) {
+            this.stack.push('spade_' + i);
+            this.stack.push('hearts_' + i);
+            this.stack.push('diamonds_' + i);
+            this.stack.push('clubs_' + i);
+        }
+
+        shuffleArray(this.stack);
+    }
+
+
+}
+
+function shuffleArray(array: any[]) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+
 }
