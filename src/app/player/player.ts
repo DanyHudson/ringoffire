fbded6dcdeab21a36fcbe7b55ebee435ce15637f
DanyHudson <mail.d.hudson@gmail.com>
2025-10-09 13:27:00 +0200

    added player names

--- src/app/player/player.ts
+++ src/app/player/player.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-player',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './player.html',
   styleUrl: './player.scss'
 })
 export class Player {
 
+  @Input() name: string = '';
+  
 }
