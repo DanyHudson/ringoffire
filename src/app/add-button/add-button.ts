c61c8b7a0992294c5f6cfbb55fb2ac5da4d7c9f1
DanyHudson <mail.d.hudson@gmail.com>
2025-10-11 22:55:03 +0200

    added angular material setup for add-button

--- src/app/add-button/add-button.ts
+++ src/app/add-button/add-button.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFabButton } from '@angular/material/button';
+import { AddPlayerDialog } from '../add-player-dialog/add-player-dialog';
 
 @Component({
   selector: 'app-add-button',
-  imports: [],
+  standalone: true,
+  imports: [MatFabButton, MatDialogModule ],
   templateUrl: './add-button.html',
-  styleUrl: './add-button.scss'
+  styleUrls: ['./add-button.scss']
 })
 export class AddButton {
+  constructor(private dialog: MatDialog) {}
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddPlayerDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        // Add the player name to your players array here
+        console.log('Player added:', result);
+      }
+    });
+  }
 }
