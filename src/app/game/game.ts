5a23eed8f89058df52d0724dee84c32750a9f3a4
DanyHudson <mail.d.hudson@gmail.com>
2025-10-28 16:16:26 +0100

    worked out angular 20+ code for logging out the game data, started on 'addDoc'

--- src/app/game/game.ts
+++ src/app/game/game.ts
@@ -4,10 +4,10 @@ import { GameData } from '../../../src/models/game-data';
 import { Player } from '../player/player';
 import { AddButton } from '../add-button/add-button';
 import { GameInfo } from "../game-info/game-info";
-// import { Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Injectable, inject, OnDestroy } from '@angular/core';
 // import { FirestoreDataService } from "../firebase-services/firestore-data.service";
-import { Firestore, collection, query, onSnapshot } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, query, onSnapshot, doc, addDoc, updateDoc, deleteDoc } from '@angular/fire/firestore';
 
 
 
@@ -22,16 +22,17 @@ import { Firestore, collection, query, onSnapshot } from '@angular/fire/firestor
 @Injectable({
   providedIn: 'root'
 })
-export class Game implements OnDestroy{
+export class Game implements OnDestroy {
   pickCardAnimation = false;
   currentCard: string | undefined = undefined;
   gameData: GameData = new GameData();
   addPlayerNote: string = '';
 
   // unsubscribeGames: () => void;
-  unsubscribeGames: () => void = () => {};
+  unsubscribeGames: () => void = () => { };
 
   firestore = inject(Firestore);
+  items$: Observable<any[]> = new Observable<any[]>();
 
   // constructor(private cdr: ChangeDetectorRef, private firestore: Firestore) {  // here 'cdr' needed to be added to be able to manually trigger the vanishing of the card
   //   this.newGame();
@@ -42,46 +43,39 @@ export class Game implements OnDestroy{
     this.newGame();
   }
 
-  // first version
-  // ngOnInit(): void {
-  //   console.log('Game component initialized');
-  //   const gamesCollection = collection(this.firestore, 'games');
-  //   collectionData(gamesCollection).subscribe((gameData: any) => {
-  //     console.log('Games from Firestore:', gameData);
-  //   });
-  // }
-
-// versuion with query
-  // ngOnInit(): void {
-  //   const gamesCollection = collection(this.firestore, 'games');
-  //   const gamesQuery = query(gamesCollection);
-  //   collectionData(gamesQuery).subscribe((gameData: any) => {
-  //     console.log('Games from Firestore:', gameData);
-  //   });
-  // }
-
 
   // version with onSnapshot
-   ngOnInit(): void {
+  ngOnInit(): void {
     const gamesCollection = collection(this.firestore, 'games');
+    console.log('collection reference:', gamesCollection);
     const q = query(gamesCollection);
-    onSnapshot(q, (gameData: any) => {
-      console.log('Games from Firestore:', gameData);
+    const docRef = this.getSingleDocRef('games', 'yfvFL0P3chWQR0A20eqr');
+    onSnapshot(docRef, (snapshot) => {
+      console.log('Document data:', snapshot.data());
     });
 
   }
 
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+
+  }
+
   ngOnDestroy(): void {
-  if (this.unsubscribeGames) {
-    this.unsubscribeGames();
+    if (this.unsubscribeGames) {
+      this.unsubscribeGames();
+    }
   }
-}
 
 
-  newGame() {
+  async newGame() {
     this.gameData;
-    // console.log(this.gameData);
+    const gamesCollection = collection(this.firestore, 'games');
+    const newGameData = {'Hello': 'World'};
+    await addDoc(gamesCollection, newGameData);
   }
+
+
 
   takeCard() {
     if (this.gameData.players.length === 0) {
